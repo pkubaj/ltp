3adbb6da0e4e161afd48975fb6acd9438da7558a
Piotr Kubaj <piotr.kubaj@intel.com>
2025-05-12 11:52:37 +0200

    turbostat: add wrappers for kselftests
    Signed-off-by: Piotr Kubaj <piotr.kubaj@intel.com>

---
+++ testcases/commands/turbostat/turbostat02.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (C) 2025 Intel - http://www.intel.com/
+#
+# GNU General Public License for more details.
+###############################################################################
+# Contributors:
+#   Piotr Kubaj <piotr.kubaj@intel.com> (Intel)
+#     -Initial draft.
+###############################################################################
+
+${TESTDIR:-/usr/src/linux/tools/testing/selftests/turbostat}/defcolumns.py
+ret=$?
+
+if [ $ret -gt 0 ]; then
+	ret=1
+fi
+
+exit $ret
